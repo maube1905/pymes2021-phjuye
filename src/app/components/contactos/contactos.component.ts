e9f741da4c580ba2914af50066924e76c20a9c02
maube1905 <maubecinagustin@gmail.com>
2021-11-09 09:18:30 -0300

    1 file modified

--- src/app/components/contactos/contactos.component.ts
+++ src/app/components/contactos/contactos.component.ts
@@ -36,7 +36,30 @@ export class ContactosComponent implements OnInit {
     private modalDialogService: ModalDialogService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.FormBusqueda = this.formBuilder.group({});
+    this.FormRegistro = this.formBuilder.group({
+      IdContacto: [0],
+      Nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(55),
+        ],
+      ],
+      FechaNacimiento: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+          ),
+        ],
+      ],
+      Telefono: ['', [Validators.required, Validators.pattern('[0-9]{13}')]],
+    });
+  }
 
   Buscar() {
     this.contactosService.getAll().subscribe((res: Contacto[]) => {
@@ -70,7 +93,31 @@ export class ContactosComponent implements OnInit {
     });
   }
 
-  Grabar() {}
+  Grabar() {
+    this.submitted = true;
+    // verificar que los validadores esten OK
+    if (this.FormRegistro.invalid) {
+      return;
+    }
+
+    //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
+    const itemCopy = { ...this.FormRegistro.value };
+
+    //convertir fecha de string dd/MM/yyyy a ISO para que la entienda webapi
+    var arrFecha = itemCopy.FechaNacimiento.substr(0, 10).split('/');
+    if (arrFecha.length == 3)
+      itemCopy.FechaNacimiento = new Date(
+        arrFecha[2],
+        arrFecha[1] - 1,
+        arrFecha[0]
+      ).toISOString();
+
+      this.contactosService.post(itemCopy).subscribe((res: any) => {
+        this.Volver();
+        this.modalDialogService.Alert('Registro agregado correctamente.');
+        this.Buscar();
+      });
+  }
 
   Volver() {
     this.AccionABMC = 'L';
